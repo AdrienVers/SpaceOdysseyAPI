5df48f2d62bcbd0b78fcf5136516d39bd7e0afbf
AdrienVers <adrienvx77@outlook.fr>
2022-11-26 19:16:53 +0100

    Addition of datas

--- app.js
+++ app.js
@@ -18,33 +18,38 @@ app.get("/", (req, res) => {
   res.json([
     {
       id: 1,
-      title: "Airbus",
-      imageUrl: "https://i.imgur.com/PLfKZrR.png",
+      name: "Airbus",
+      logo: "https://i.imgur.com/PLfKZrR.png",
       price: 114,
+      nextPrice: 120,
     },
     {
       id: 2,
-      title: "Sanofi",
-      imageUrl: "https://i.imgur.com/AliKjXr.png",
+      name: "Sanofi",
+      logo: "https://i.imgur.com/AliKjXr.png",
       price: 87,
+      nextPrice: 90,
     },
     {
       id: 3,
-      title: "Deutsche Telekom",
-      imageUrl: "https://i.imgur.com/hHLBfKt.png",
+      name: "Deutsche Telekom",
+      logo: "https://i.imgur.com/hHLBfKt.png",
       price: 19,
+      nextPrice: 15,
     },
     {
       id: 4,
-      title: "Unilever",
-      imageUrl: "https://i.imgur.com/kQnGM6p.png",
+      name: "Unilever",
+      logo: "https://i.imgur.com/kQnGM6p.png",
       price: 410,
+      nextPrice: 400,
     },
     {
       id: 5,
-      title: "Repsol",
-      imageUrl: "https://i.imgur.com/w59TeaS.png",
+      name: "Repsol",
+      logo: "https://i.imgur.com/w59TeaS.png",
       price: 14,
+      nextPrice: 40,
     },
   ]);
 });
