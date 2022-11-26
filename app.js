63ace14b6b771dc7064d4dc4a80041cb0e3f8ac3
AdrienVers <adrienvx77@outlook.fr>
2022-11-26 14:07:34 +0100

    Modification of JSON file

--- app.js
+++ app.js
@@ -15,24 +15,20 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  res.json({
-    items: [
-      [
-        {
-          id: 1,
-          title: "Airbus",
-          imageUrl: "https://i.imgur.com/PLfKZrR.png",
-          price: 114,
-        },
-        {
-          id: 2,
-          title: "Sanofi",
-          imageUrl: "https://i.imgur.com/AliKjXr.png",
-          price: 87,
-        },
-      ],
-    ],
-  });
+  res.json([
+    {
+      id: 1,
+      title: "Airbus",
+      imageUrl: "https://i.imgur.com/PLfKZrR.png",
+      price: 114,
+    },
+    {
+      id: 2,
+      title: "Sanofi",
+      imageUrl: "https://i.imgur.com/AliKjXr.png",
+      price: 87,
+    },
+  ]);
 });
 
 module.exports = app;
