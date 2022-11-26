e3505dcf237fb846e6e465c095a7f5c295e67d7f
AdrienVers <adrienvx77@outlook.fr>
2022-11-26 12:48:03 +0100

    Export DistrictMoneyBackend to GitHub

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.json({
+    items: [
+      {
+        id: 1,
+        name: "Airbus",
+        logo: "https://i.imgur.com/PLfKZrR.png",
+        price: 114,
+      },
+      {
+        id: 2,
+        name: "Sanofi",
+        logo: "https://i.imgur.com/AliKjXr.png",
+        price: 87,
+      },
+    ],
+  });
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http'); 
+const app = require('./app'); 
+const port = normalizePort('8000');
+
+app.set('port', port); 
+const server = http.createServer(app);
+
+app.listen(port, () => { console.log("Server stated on port 8000")})
+
+function normalizePort(val) {
+    var port = parseInt(val, 10)
+
+    if (isNaN(port)) { return val }
+    if (port >= 0) { return port }
+    return false }
