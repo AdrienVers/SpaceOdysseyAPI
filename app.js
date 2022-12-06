776459cd57a098bb1201786b4e6125009a1a0b51
AdrienVers <adrienvx77@outlook.fr>
2022-12-06 21:12:50 +0100

    update of json file

--- app.js
+++ app.js
@@ -15,53 +15,55 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  res.json([
-    {
-      id: 1,
-      name: "Airbus",
-      logo: "https://i.imgur.com/PLfKZrR.png",
-      price: 114,
-      nextPrice: 120,
-      market: "EURONEXT PARIS",
-      sector: "Industrie",
-    },
-    {
-      id: 2,
-      name: "Sanofi",
-      logo: "https://i.imgur.com/AliKjXr.png",
-      price: 87,
-      nextPrice: 90,
-      market: "EURONEXT PARIS",
-      sector: "Santé",
-    },
-    {
-      id: 3,
-      name: "Deutsche Telekom",
-      logo: "https://i.imgur.com/hHLBfKt.png",
-      price: 19,
-      nextPrice: 15,
-      market: "DEUTSCHE BOERSE AG",
-      sector: "Télécom",
-    },
-    {
-      id: 4,
-      name: "Unilever",
-      logo: "https://i.imgur.com/kQnGM6p.png",
-      price: 410,
-      nextPrice: 400,
-      market: "LONDON STOCK EXCHANGE",
-      sector: "Consommation de base",
-    },
-    {
-      id: 5,
-      name: "Repsol",
-      logo: "https://i.imgur.com/w59TeaS.png",
-      price: 14,
-      nextPrice: 40,
-      market: "BOLSA DE MADRID",
-      sector: "Énergie",
-    },
-  ]);
+  res.json({
+    results: [
+      {
+        id: 1,
+        name: "Airbus",
+        logo: "https://i.imgur.com/PLfKZrR.png",
+        price: 114,
+        nextPrice: 120,
+        market: "EURONEXT PARIS",
+        sector: "Industrie",
+      },
+      {
+        id: 2,
+        name: "Sanofi",
+        logo: "https://i.imgur.com/AliKjXr.png",
+        price: 87,
+        nextPrice: 90,
+        market: "EURONEXT PARIS",
+        sector: "Santé",
+      },
+      {
+        id: 3,
+        name: "Deutsche Telekom",
+        logo: "https://i.imgur.com/hHLBfKt.png",
+        price: 19,
+        nextPrice: 15,
+        market: "DEUTSCHE BOERSE AG",
+        sector: "Télécom",
+      },
+      {
+        id: 4,
+        name: "Unilever",
+        logo: "https://i.imgur.com/kQnGM6p.png",
+        price: 410,
+        nextPrice: 400,
+        market: "LONDON STOCK EXCHANGE",
+        sector: "Consommation de base",
+      },
+      {
+        id: 5,
+        name: "Repsol",
+        logo: "https://i.imgur.com/w59TeaS.png",
+        price: 14,
+        nextPrice: 40,
+        market: "BOLSA DE MADRID",
+        sector: "Énergie",
+      },
+    ],
+  });
 });
 
 module.exports = app;
