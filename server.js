43676d6a3697d9b74fb6f526a7630817dde50a77
AdrienVers <adrienvx77@outlook.fr>
2023-02-27 22:54:26 +0100

    push to github

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const http = require('http'); 
-const app = require('./app'); 
-const port = normalizePort('8000');
+const http = require("http");
+const app = require("./app");
+const port = normalizePort("8000");
 
-app.set('port', port); 
+app.set("port", port);
 const server = http.createServer(app);
 
-app.listen(port, () => { console.log("Server stated on port 8000")})
+app.listen(port, () => {
+	console.log("Server stated on port 8000");
+});
 
 function normalizePort(val) {
-    var port = parseInt(val, 10)
+	var port = parseInt(val, 10);
 
-    if (isNaN(port)) { return val }
-    if (port >= 0) { return port }
-    return false }
+	if (isNaN(port)) {
+		return val;
+	}
+	if (port >= 0) {
+		return port;
+	}
+	return false;
+}
